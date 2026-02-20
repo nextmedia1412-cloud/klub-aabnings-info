a38d5d124b7350f2c8645cc170d267f8d9ba6a4a
nextmedia1412-cloud <nextmedia1412@gmail.com>
2026-02-20 20:00:48 +0100

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "klubinfo-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request).catch(() => caches.match("./")))
+  );
+});
